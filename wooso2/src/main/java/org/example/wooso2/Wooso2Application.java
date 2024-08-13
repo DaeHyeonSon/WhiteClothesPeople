a89e3a39b7a006b6d600dfbbe7f5774c8189b310
ayleeee <janhyang0309@gmail.com>
2024-08-13 12:20:21 +0900

    [CHORE] 프로젝트 구조 설정

---
+++ wooso2/src/main/java/org/example/wooso2/Wooso2Application.java
@@ -0,0 +1,13 @@
+package org.example.wooso2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Wooso2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Wooso2Application.class, args);
+    }
+
+}
---
+++ wooso2/src/test/java/org/example/wooso2/Wooso2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.wooso2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Wooso2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
