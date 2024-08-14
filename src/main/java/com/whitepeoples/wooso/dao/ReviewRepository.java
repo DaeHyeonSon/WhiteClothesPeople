7b43f77ffa754bc4bae74b20544096b5348df024
haewoni <suzyhw96@gmail.com>
2024-08-14 21:04:22 +0900

    [FEAT] - review 기능 작성

--- src/main/java/com/whitepeoples/wooso/dao/ReviewRepository.java
+++ src/main/java/com/whitepeoples/wooso/dao/ReviewRepository.java
@@ -1,24 +1,26 @@
 package com.whitepeoples.wooso.dao;
 
+import java.util.List;
 import java.util.Optional;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.whitepeoples.wooso.model.entity.Review;
+import com.whitepeoples.wooso.model.entity.User;
 
+@Repository
 public interface ReviewRepository extends JpaRepository<Review, Integer> {
 
-    // 범위 조회
-    Page<Review> findAll(Pageable pageable);
+	// 전체 조회
+	List<Review> findByReviewer(User reviewer);
 
-    // 단일 조회
-    Optional<Review> findByReviewId(Integer id);
+	// 단일 조회
+	Optional<Review> findByReviewId(Integer id);
 
-    // 필드를 통한 조회
-    Optional<Review> findByRating(Integer rating);
+	// 필드를 통한 조회
+	Optional<Review> findByRating(Integer rating);
 
-    // 필드를 통한 삭제
-   // void deleteByRating(Integer rating);
+	// 필드를 통한 삭제
+	// void deleteByRating(Integer rating);
 }
